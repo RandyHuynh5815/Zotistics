647db581ae8fea925f800211945072274718a88b
Daniel Binoy <dbinoy15@gmail.com>
2020-12-06 17:53:11 -0800

    dropdowns lol

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Container, Form, Row, Col, Button} from 'react-bootstrap';
 import Home from '../Home/Home'
 import Data from '../Data/Data'
-
+import SearchForm from './SearchForm'
 const HOME = <Home />;
 const DATA = <Data />;
 
-export default class Search extends React.Component {
+class Search extends React.Component {
     state = {
         instructor: '',
         quarters: [],
@@ -43,35 +43,12 @@ export default class Search extends React.Component {
     render() {
         return (
             <Container>
-                <Form onSubmit={this.handleSubmit}>
-                    <Row className="justify-content-center">
-                        <Col className="col-sm-5">
-                            <Form.Control type="text" name="instructor" placeholder="Instructor Name" />
-                        </Col>
-                        <Col className="col-sm-3">
-                            <Form.Control type="text" name="quarters" placeholder="Quarters" />
-                        </Col>
-                        <Col className="col-sm-3">
-                            <Form.Control type="text" name="years" placeholder="School Year" />
-                        </Col>
-                    </Row>
-
-                    <Row className="justify-content-center">
-                        <Col />
-                        <Col>
-                            <Row className="justify-content-center">
-                                <Col>
-                                    <Form.Group className="text-center">
-                                        <Button as="input" type="submit" name="submit" value="Submit" />
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col />
-                    </Row>
-                </Form>
+                <SearchForm handleSubmit={this.handleSubmit} />
                 {this.state.page}
             </Container>
         );
     }
 }
+
+
+export default Search;
