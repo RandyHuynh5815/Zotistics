5e7dfc3aa7fffaceb5cd2db4b32c4d18f4cf3952
Maybe14 <jchowdhury36@gmail.com>
2021-02-11 02:12:52 -0800

    added instructor side list

--- client/src/Components/Data/Data.js
+++ client/src/Components/Data/Data.js
@@ -1,13 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Row, Col} from "react-bootstrap";
 import Chart from "chart.js";
 
-export default function Data(props) {
+export default class Data extends React.Component {
+    constructor(props){
+        super(props);
 
-    useEffect(() => {
-        let ctx = document.getElementById('myChart').getContext('2d');
         let gradeListPopulation = [props.data.a, props.data.b, props.data.c, props.data.d, props.data.f,
-                         props.data.p, props.data.np]
+            props.data.p, props.data.np]
         const sum = gradeListPopulation.reduce(function(a, b){
             return a + b;
         });
@@ -15,13 +15,23 @@ export default function Data(props) {
             return ((grade/sum)*100).toFixed(1);
         })
 
+        this.state = {
+            gradeListPercentage: gradeListPercentage,
+            gradeListPopulation: gradeListPopulation,
+            instructorDisplay: "inherit"
+        }
+    }
+
+    componentDidMount() {
+        let ctx = document.getElementById('myChart').getContext('2d');
+
         new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: ['A', 'B', 'C', 'D', 'F', 'P', 'NP'],
                 datasets: [{
                     label: "Percent",
-                    data: gradeListPercentage,
+                    data: this.state.gradeListPercentage,
                     backgroundColor: [
                         'rgba(54, 162, 235, 0.6)',
                         'rgba(54, 162, 235, 0.6)',
@@ -57,43 +67,68 @@ export default function Data(props) {
                 }
             }
         });
-    }, [])
+    }
 
-    let title = props.data.department + " " + props.data.classNumber + " " + props.data.instructor;
-    let instructorAmount = Object.keys(props.data.instructors).length;
+    displayInstructorList = (e) => {
+        if(this.state.instructorDisplay === "none"){
+            this.setState({instructorDisplay: "inherit"})
+        } else {
+            this.setState({instructorDisplay: "none"})
+        }
+    }
 
-    return (
-        <>
-            <Row>
-                <Col sm={2}></Col>
-                <Col sm={8}>
-                    <h5 className="text-center">{title}</h5>
-                    <Row className="justify-content-between d-flex">
-                        <div className="flex-even">
-                            {instructorAmount} Instructors
+    render() {
+        let title = this.props.data.department + " " + this.props.data.classNumber + " " + this.props.data.instructor;
+        let instructorAmount = Object.keys(this.props.data.instructors).length;
+        return (
+            <>
+                <Row>
+                    {/* Instructor Side List */}
+                    <Col sm={2} className="justify-content-center text-center px-0">
+                        <div className="card overflow-auto" style={{display: this.state.instructorDisplay}} id="profList">
+                            <div className="card-body px-0">
+                                <h5 className="card-title">Instructors</h5>
+                                {Object.entries(this.props.data.instructors).map(([key, value]) => {
+                                    return(
+                                        <p className="card-text text-decoration-none" style={{color: "#212529"}}>{key} • {value}</p>
+                                    )
+                                })}
+                            </div>
                         </div>
-                        <div className="flex-even text-center">
-                            <h6>Quarter Year</h6>
-                        </div>
-                        <div className="flex-even text-right">
-                            {props.data.count} Classes
-                        </div>
-                    </Row>
-                    <Row>
-                        <Col sm={12}>
-                            <canvas id="myChart"></canvas>
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-center">
-                        <Col sm={3}></Col>
-                        <Col sm={6} className="text-center">
-                            <p>Average GPA: {props.data.averageGPA}</p>
-                        </Col>
-                        <Col sm={3}></Col>
-                    </Row>
-                </Col>
-                <Col sm={2}></Col>
-            </Row>
-        </>
-    )
+                    </Col>
+                    <Col sm={8}>
+                        {/* Headers */}
+                        <h5 className="text-center">{title}</h5>
+                        <Row className="justify-content-between d-flex">
+                            <div className="flex-even">
+                                <a id="instructors" onClick={this.displayInstructorList} style={{cursor: "pointer", userSelect: "none"}}><span style={{fontFamily: "Symbola"}}>&#x2B9C;</span> <u>{instructorAmount} Instructors</u></a>
+                            </div>
+                            <div className="flex-even text-center">
+                                <h6>Quarter Year</h6>
+                            </div>
+                            <div className="flex-even text-right">
+                                {this.props.data.count} Classes
+                            </div>
+                        </Row>
+                        {/* Graph */}
+                        <Row>
+                            <Col sm={12}>
+                                <canvas id="myChart"></canvas>
+                            </Col>
+                        </Row>
+                        {/* Buttons and GPA */}
+                        <Row className="justify-content-center">
+                            <Col sm={3}></Col>
+                            <Col sm={6} className="text-center">
+                                <p>Average GPA: {this.props.data.averageGPA}</p>
+                            </Col>
+                            <Col sm={3}></Col>
+                        </Row>
+                    </Col>
+                    {/* Class Side List*/}
+                    <Col sm={2}></Col>
+                </Row>
+            </>
+        )
+    }
 }
