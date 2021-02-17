d224a531e9e61435c49fccff38909dc8fc57742d
Daniel Binoy <dbinoy15@gmail.com>
2021-02-17 03:47:07 -0800

    restructure search to handle multiple forms

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,72 +1,172 @@
-import React from 'react';
-import {Container} from 'react-bootstrap';
-import Home from '../Home/Home'
-import Data from '../Data/Data'
-import SearchForm from './SearchForm'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Home from "../Home/Home";
+import Data from "../Data/Data";
+import SearchForm from "./SearchForm";
 const HOME = <Home />;
 let DATA = <Data />;
 
+const DEFAULT_STATE = {
+  formID: 0,
+  instructor: "",
+  quarters: [],
+  years: [],
+  department: "",
+  classNumber: "",
+  classCode: "",
+  advancedVisible: false,
+  excludePNP: false,
+  covid19: false,
+  lowerDiv: false,
+  upperDiv: false,
+};
+
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instructors: [],
+      forms: {},
+      formStates: {},
+      currentForm: 0,
+      numForms: 0,
+      page: HOME, // what the page will display below the search forms (HOME or DATA)
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            instructor: '',
-            quarters: [],
-            years: [],
-            department: '',
-            classNumber: '',
-            classCode: '',
-            passNoPass: false,
-            covid: false,
-            lowerDiv: false,
-            upperDiv: false,
-            page: HOME, // what the page will display below the search forms (HOME or DATA)
-            instructors: []
-        };
-    }
-    
-    componentDidMount() {
-        fetch('/instructors')
-            .then(res => res.json())
-            .then(res => this.setState({instructors: res.instructors.map(teacher => ({name: teacher, value: teacher}))}))
-    }
-
-    handleValueChange = (e) => {
-        this.setState({[e.name]: e.value});
-        console.log(e.name+ " : "+e.value);
-    }
-
-    handleFormSubmit = (e) =>{
-        console.log("form submit called");
-        e.preventDefault();
-        fetch('/search', {
-            method: 'post',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(this.state)
+  componentDidMount() {
+    fetch("/instructors")
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          instructors: res.instructors.map((teacher) => ({
+            name: teacher,
+            value: teacher,
+          })),
         })
-        .then(res => res.json())
-        .then(data => {
-            DATA = <Data data={data} />
-            this.setState({page: DATA});
-        })
-    }
+      )
+      .then(() => this.addNewForm());
+  }
 
-    render() {
-        return (
-            <Container>
-                <SearchForm 
-                    handleValueChange={this.handleValueChange} 
-                    handleFormSubmit={this.handleFormSubmit}
-                    instructors={this.state.instructors}
+
+  setCurrentForm = (key, e) => {
+    e.preventDefault(); //ignore link behavior
+    this.setState({
+      currentForm : key
+    },
+    function(){
+      this.forceUpdate()
+    }
+    )
+  }
+
+  addNewForm = () => {
+    this.setState(
+      {
+        numForms: this.state.numForms + 1,
+        currentForm: this.state.numForms + 1,
+      },
+      function () {
+        this.setState({
+          forms: {
+            ...this.state.forms,
+            [this.state.currentForm]: (
+              <SearchForm
+                handleFormSubmit={this.handleFormSubmit}
+                updateForm={this.updateForm}
+                formID={this.state.currentForm}
+                instructors={this.state.instructors}
+              ></SearchForm>
+            ),
+          },
+          formStates: {
+            ...this.state.formStates,
+            [this.state.formStates]: DEFAULT_STATE,
+          },
+        });
+      }
+    );
+  };
+
+  updateForm = (formState) => {
+    console.log("setting form " + formState.formID);
+
+    this.setState(
+      {
+        formStates: {
+          ...this.state.formStates,
+          [formState.formID]: formState,
+        },
+      },
+      function () {
+        console.log(this.state.formStates);
+      }
+    );
+  };
+
+  handleFormSubmit = (e) => {
+    console.log("form submitted");
+    e.preventDefault();
+
+    fetch("/search", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        DATA = <Data data={data} />;
+        this.setState({ page: DATA });
+      });
+  };
+
+  render() {
+    let { forms, currentForm, formStates } = this.state;
+
+    console.log(Object.keys(formStates));
+    return (
+      <Container>
+        <Form onSubmit={this.handleFormSubmit}>
+          <p>Current Form: {this.state.currentForm}</p>
+
+          {currentForm !== 0 ? forms[currentForm] : null}
+          <Row className="justify-content-center search-form-row">
+            <Col>
+              <Form.Group className="text-center">
+                <Button
+                  id="submit-button"
+                  as="input"
+                  type="submit"
+                  name="submit"
+                  value="Submit"
                 />
-                {this.state.page}
-            </Container>
-        );
-    }
-}
+              </Form.Group>
+            </Col>
+            <Col>
+              <Button onClick={this.addNewForm}>+</Button>
+            </Col>
+          </Row>
 
+          <Row>
+            {Object.keys(formStates).map((key) => {
+              return (
+                <Col>
+                  <a href="#" onClick={(e)=>this.setCurrentForm(key, e)}> {formStates[key].instructor}</a>
+                </Col>
+              );
+            })}
+          </Row>
+        </Form>
+        {this.state.page}
+      </Container>
+    );
+  }
+}
 
 export default Search;
--- client/src/Components/Search/SearchForm.js
+++ client/src/Components/Search/SearchForm.js
@@ -1,195 +1,257 @@
-import React from 'react';
-import { Form, Row, Col, Button, Container, Collapse } from 'react-bootstrap';
-import SelectSearch from 'react-select-search';
-import './searchform.css';
-const dep = require('./departments')
+import React from "react";
+import { Form, Row, Col, Button, Container, Collapse } from "react-bootstrap";
+import SelectSearch from "react-select-search";
+import "./searchform.css";
+const dep = require("./departments");
 
 const quarters = [
-    { name: 'Fall', value: 'Fall' },
-    { name: 'Winter', value: 'Winter' },
-    { name: 'Spring', value: 'Spring' },
-    { name: 'Summer', value: 'Summer' },
+  { name: "Fall", value: "Fall" },
+  { name: "Winter", value: "Winter" },
+  { name: "Spring", value: "Spring" },
+  { name: "Summer", value: "Summer" },
 ];
 
 const years = [
-    { name: '2020-2021', value: '2020-21' },
-    { name: '2019-2020', value: '2019-20' },
-    { name: '2018-2019', value: '2018-19' },
-    { name: '2017-2018', value: '2017-18' },
-    { name: '2016-2017', value: '2016-17' },
-    { name: '2015-2016', value: '2015-16' },
-    { name: '2014-2015', value: '2014-15' },
+  { name: "2020-2021", value: "2020-21" },
+  { name: "2019-2020", value: "2019-20" },
+  { name: "2018-2019", value: "2018-19" },
+  { name: "2017-2018", value: "2017-18" },
+  { name: "2016-2017", value: "2016-17" },
+  { name: "2015-2016", value: "2015-16" },
+  { name: "2014-2015", value: "2014-15" },
 ];
 
 const departments = dep.departments;
 
-
-function RenderOptions({options}){
-
-}
-
 class SearchForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleValueChange = props.handleValueChange;
-        this.handleFormSubmit = props.handleFormSubmit;
-        this.state = {
-            advancedVisible: false,
-            excludePNP : false,
-            covid19 : false,
-            lowerDiv: false,
-            upperDiv: false
-        };
+  constructor(props) {
+    super(props);
+    this.handleFormSubmit = props.handleFormSubmit;
+    this.state = {
+      formID: props.formID,
+      instructor: "",
+      quarters: [],
+      years: [],
+      department: "",
+      classNumber: "",
+      classCode: "",
+      advancedVisible: false,
+      excludePNP: false,
+      covid19: false,
+      lowerDiv: false,
+      upperDiv: false,
+    };
+
+  }
+
+  handleValueChange = (e) => {
+    this.setState({ [e.name]: e.value }, function () {
+      this.props.updateForm(this.state);
+    });
+  };
+
+  render() {
+    let instructors = [];
+
+    if (this.props.instructors !== []) {
+      instructors = this.props.instructors;
     }
 
-    render() {
-        let instructors = []
-
-        if(this.props.instructors !== []){
-            instructors = this.props.instructors;
-        }
-
-        return (
-            <Form onSubmit={this.handleFormSubmit}>
-                <Container>
-                    <Row className="justify-content-center search-form-row">
-                        <Col className="col-12 col-sm-12 col-md-5">
-                            <SelectSearch
-                                options={instructors}
-                                filterOptions={(q, options) => {
-                                    return options.slice(0, 4);
-                                }}
-                                search
-                                name="instructors"
-                                onChange={(val) => this.handleValueChange({ name: "instructor", value: val })}
-                                placeholder="Instructor Name"
-                            />
-                        </Col>
-                        <Col className="col-12 col-sm-12 col-md-3">
-                            <SelectSearch
-                                closeOnSelect={false}
-                                printOptions="on-focus"
-                                multiple
-                                name="quarters"
-                                onChange={(val) => this.handleValueChange({ name: "quarters", value: val })}
-                                placeholder="Quarters"
-                                options={quarters}
-                            />
-                        </Col>
-                        <Col className="col-12 col-sm-12 col-md-3">
-                            <SelectSearch
-                                closeOnSelect={false}
-                                printOptions="on-focus"
-                                name="years"
-                                onChange={(val) => this.handleValueChange({ name: "years", value: val })}
-                                multiple
-                                placeholder="Years"
-                                options={years}
-                            />
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-center search-form-row">
-                        <Col className="col-12 col-sm-12 col-md-5">
-                            <SelectSearch
-                                options={departments}
-                                search
-                                name="department"
-                                onChange={(val) => this.handleValueChange({ name: "department", value: val })}
-                                placeholder="All Departments"
-                            />
-                        </Col>
-                        <Col className="col-12 col-sm-12 col-md-3">
-                            <Form.Control
-                                onChange={(val) => this.handleValueChange({ name: "classNumber", value: val.target.value })}
-                                className="search-text-box" type="text" placeholder="Class Number (Ex. 45C)" />
-                        </Col>
-                        <Col className="col-12 col-sm-12 col-md-3">
-                            <Form.Control
-                                onChange={(val) => this.handleValueChange({ name: "classCode", value: val.target.value })}
-                                className="search-text-box" type="text" placeholder="Class Code (Ex. 34060)" />
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-center search-form-row">
-                        <Col className="col-12">
-                            <div className = "advanced-options-wrapper">
-                                <a  id = "advanced-options-link"
-                                    onClick={() => this.setState({advancedVisible:!this.state.advancedVisible}) }
-                                    aria-controls="example-collapse-text"
-                                    aria-expanded={this.state.advancedVisible}>
-                                    Advanced Options ▼
-                                </a>
-                                <Collapse in={this.state.advancedVisible}>
-                                    <div className="advanced-options">
-                                        <Container>
-                                            <Row  className="justify-content-center text-center">
-                                                <Col >
-                                                    <Form.Check
-                                                        checked = {this.state.excludePNP}
-                                                        onChange = {(evt) => {
-                                                            this.setState({excludePNP: evt.target.checked})
-                                                            this.handleValueChange({ name: "passNoPass", value: !this.state.excludePNP})
-                                                        } }
-                                                        type="checkbox"
-                                                        id={`exclude-pnp`}
-                                                        label={`Exclude P/NP`}
-                                                    />
-                                                </Col>
-                                                <Col>
-                                                    <Form.Check
-                                                        checked = {this.state.covid19}
-                                                        onChange = {(evt) => {
-                                                            this.setState({covid19: evt.target.checked})
-                                                            this.handleValueChange({ name: "covid", value: !this.state.covid19})
-                                                        } }
-                                                        type="checkbox"
-                                                        id={`exclude-covid-19`}
-                                                        label={`Exclude COVID-19`}
-                                                    />
-                                                </Col>
-                                                <Col>
-                                                    <Form.Check
-                                                        checked = {this.state.lowerDiv}
-                                                        onChange = {(evt) => {
-                                                            this.setState({lowerDiv: evt.target.checked})
-                                                            this.handleValueChange({ name: "lowerDiv", value: !this.state.lowerDiv})
-                                                        } }
-                                                        type="checkbox"
-                                                        id={`lower-div-only`}
-                                                        label={`LowerDiv Only`}
-                                                    />
-                                                </Col>
-                                                <Col>
-                                                    <Form.Check
-                                                        checked = {this.state.upperDiv}
-                                                        onChange = {(evt) => {
-                                                            this.setState({upperDiv: evt.target.checked})
-                                                            this.handleValueChange({ name: "upperDiv", value: !this.state.upperDiv})
-                                                        } }
-                                                        type="checkbox"
-                                                        id={`upper-div-only`}
-                                                        label={`UpperDiv Only`}
-                                                    />
-                                                </Col>
-                                            </Row>
-                                        </Container>
-                                    </div>
-                                </Collapse>
-                            </div>
-                        </Col>
-                    </Row>
-
-                    <Row className="justify-content-center search-form-row">
+    return (
+      
+        <Container>
+          <Row className="justify-content-center search-form-row">
+            <Col className="col-12 col-sm-12 col-md-5">
+              <SelectSearch
+                options={instructors}
+                filterOptions={(q, options) => {
+                  return options.slice(0, 4);
+                }}
+                search
+                name="instructors"
+                onChange={(val) =>
+                  this.handleValueChange({
+                    name: "instructor",
+                    value: val,
+                    formID: this.props.formID,
+                  })
+                }
+                placeholder="Instructor Name"
+              />
+            </Col>
+            <Col className="col-12 col-sm-12 col-md-3">
+              <SelectSearch
+                closeOnSelect={false}
+                printOptions="on-focus"
+                multiple
+                name="quarters"
+                onChange={(val) =>
+                  this.handleValueChange({
+                    name: "quarters",
+                    value: val,
+                    formID: this.props.formID,
+                  })
+                }
+                placeholder="Quarters"
+                options={quarters}
+              />
+            </Col>
+            <Col className="col-12 col-sm-12 col-md-3">
+              <SelectSearch
+                closeOnSelect={false}
+                printOptions="on-focus"
+                name="years"
+                onChange={(val) =>
+                  this.handleValueChange({
+                    name: "years",
+                    value: val,
+                    formID: this.props.formID,
+                  })
+                }
+                multiple
+                placeholder="Years"
+                options={years}
+              />
+            </Col>
+          </Row>
+          <Row className="justify-content-center search-form-row">
+            <Col className="col-12 col-sm-12 col-md-5">
+              <SelectSearch
+                options={departments}
+                search
+                name="department"
+                onChange={(val) =>
+                  this.handleValueChange({
+                    name: "department",
+                    value: val,
+                    formID: this.props.formID,
+                  })
+                }
+                placeholder="All Departments"
+              />
+            </Col>
+            <Col className="col-12 col-sm-12 col-md-3">
+              <Form.Control
+                onChange={(val) =>
+                  this.handleValueChange({
+                    name: "classNumber",
+                    value: val.target.value,
+                    formID: this.props.formID,
+                  })
+                }
+                className="search-text-box"
+                type="text"
+                placeholder="Class Number (Ex. 45C)"
+              />
+            </Col>
+            <Col className="col-12 col-sm-12 col-md-3">
+              <Form.Control
+                onChange={(val) =>
+                  this.handleValueChange({
+                    name: "classCode",
+                    value: val.target.value,
+                    formID: this.props.formID,
+                  })
+                }
+                className="search-text-box"
+                type="text"
+                placeholder="Class Code (Ex. 34060)"
+              />
+            </Col>
+          </Row>
+          <Row className="justify-content-center search-form-row">
+            <Col className="col-12">
+              <div className="advanced-options-wrapper">
+                <a
+                  id="advanced-options-link"
+                  onClick={() =>
+                    this.setState({
+                      advancedVisible: !this.state.advancedVisible,
+                    })
+                  }
+                  aria-controls="example-collapse-text"
+                  aria-expanded={this.state.advancedVisible}
+                >
+                  Advanced Options ▼
+                </a>
+                <Collapse in={this.state.advancedVisible}>
+                  <div className="advanced-options">
+                    <Container>
+                      <Row className="justify-content-center text-center">
                         <Col>
-                            <Form.Group className="text-center">
-                                <Button id="submit-button" as="input" type="submit" name="submit" value="Submit" />
-                            </Form.Group>
+                          <Form.Check
+                            checked={this.state.excludePNP}
+                            onChange={(evt) => {
+                              this.setState({ excludePNP: evt.target.checked });
+                              this.handleValueChange({
+                                name: "passNoPass",
+                                value: !this.state.excludePNP,
+                                formID: this.props.formID,
+                              });
+                            }}
+                            type="checkbox"
+                            id={`exclude-pnp`}
+                            label={`Exclude P/NP`}
+                          />
                         </Col>
-                    </Row>
-                </Container>
-            </Form>
-        );
-    }
-
+                        <Col>
+                          <Form.Check
+                            checked={this.state.covid19}
+                            onChange={(evt) => {
+                              this.setState({ covid19: evt.target.checked });
+                              this.handleValueChange({
+                                name: "covid",
+                                value: !this.state.covid19,
+                                formID: this.props.formID,
+                              });
+                            }}
+                            type="checkbox"
+                            id={`exclude-covid-19`}
+                            label={`Exclude COVID-19`}
+                          />
+                        </Col>
+                        <Col>
+                          <Form.Check
+                            checked={this.state.lowerDiv}
+                            onChange={(evt) => {
+                              this.setState({ lowerDiv: evt.target.checked });
+                              this.handleValueChange({
+                                name: "lowerDiv",
+                                value: !this.state.lowerDiv,
+                                formID: this.props.formID,
+                              });
+                            }}
+                            type="checkbox"
+                            id={`lower-div-only`}
+                            label={`LowerDiv Only`}
+                          />
+                        </Col>
+                        <Col>
+                          <Form.Check
+                            checked={this.state.upperDiv}
+                            onChange={(evt) => {
+                              this.setState({ upperDiv: evt.target.checked });
+                              this.handleValueChange({
+                                name: "upperDiv",
+                                value: !this.state.upperDiv,
+                                formID: this.props.formID,
+                              });
+                            }}
+                            type="checkbox"
+                            id={`upper-div-only`}
+                            label={`UpperDiv Only`}
+                          />
+                        </Col>
+                      </Row>
+                    </Container>
+                  </div>
+                </Collapse>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+    );
+  }
 }
 
 export default SearchForm;
