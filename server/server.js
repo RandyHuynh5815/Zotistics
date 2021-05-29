6d254b0f2008bec1c24d15a11698659775fbc482
Maybe14 <jchowdhury36@gmail.com>
2021-05-28 17:41:07 -0700

    graphql

--- server/server.js
+++ server/server.js
@@ -32,46 +32,76 @@ app.use('/instructors', (req, res) => {
         });
 })
 
-//app.use('/cachedinstructors', (req, res) => res.json(instructors));
+function searchQuery(args){
+    return `
+        query {
+          grades(${args}) {
+            grade_distributions{
+              course_offering{
+                year
+                quarter
+                instructors{
+                  name
+                }
+                section{
+                  code
+                }
+                course {
+                  department
+                  number
+                  title
+                }
+              }
+            }
+          }
+        }
+        `
+}
 
 app.use('/search', (req, res) => {
     let params = req.body;
     let quarters = params.quarters.join(';');
     let years = params.years.join(';');
+    let code = (params.classCode !== '') ? parseFloat(params.classCode) : null
     let department = params.department.replace('&', '%26').replace('/', '%2');
-    let query = `instructor=${params.instructor}&quarter=${quarters}&year=${years}&department=${department}` +
-        `&number=${params.classNumber}&code=${params.classCode}`
+    let args = `instructor: "${params.instructor}", quarter: "${quarters}", year: "${years}", department: "${department}",
+        number: "${params.classNumber}", code: ${code}`
 
-
-    fetch(`http://api.peterportal.org/rest/v0/grades/calculated?${query}`, {
+    fetch(url, {
+        body: JSON.stringify({"query": searchQuery(args)}),
+        method: 'POST',
         headers: {
+            'Content-Type': 'application/json',
             'x-api-key': 'Zotistics-48e7d5db47d3bf0ebfef45fe0aea7b3df77d0c77b243ee4bc9b780df6c9dd91f'
         }})
         .then(res => res.json())
         .then(data => {
-            let classes = calc.classList(data['courseList']);
-            let instructors = calc.instructorList(data['courseList']);
-
-            let count = data['gradeDistribution']['COUNT()']
-            let a = data['gradeDistribution']['SUM(gradeACount)']
-            let b = data['gradeDistribution']['SUM(gradeBCount)']
-            let c = data['gradeDistribution']['SUM(gradeCCount)']
-            let d = data['gradeDistribution']['SUM(gradeDCount)']
-            let f = data['gradeDistribution']['SUM(gradeFCount)']
-            let p = data['gradeDistribution']['SUM(gradePCount)']
-            let np = data['gradeDistribution']['SUM(gradeNPCount)']
-            let averageGPA = data['gradeDistribution']['AVG(averageGPA)'].toFixed(2)
-
-            //sends json as response
-            res.json({count: count, a: a, b: b, c: c, d: d, f: f, p: p, np: np,
-                      averageGPA: averageGPA, classes: classes, instructors: instructors,
-                      instructor: params.instructor, quarters: params.quarters,
-                      department: params.department, classNumber: params.classNumber,
-                      classCode: params.classCode, courseList: data['courseList']
-            });
+            console.log(data.data.grades.grade_distributions)
+            res.send({data})
         });
+    // .then(data => {
+    //     let classes = calc.classList(data['courseList']);
+    //     let instructors = calc.instructorList(data['courseList']);
+    //
+    //     let count = data['gradeDistribution']['COUNT()']
+    //     let a = data['gradeDistribution']['SUM(gradeACount)']
+    //     let b = data['gradeDistribution']['SUM(gradeBCount)']
+    //     let c = data['gradeDistribution']['SUM(gradeCCount)']
+    //     let d = data['gradeDistribution']['SUM(gradeDCount)']
+    //     let f = data['gradeDistribution']['SUM(gradeFCount)']
+    //     let p = data['gradeDistribution']['SUM(gradePCount)']
+    //     let np = data['gradeDistribution']['SUM(gradeNPCount)']
+    //     let averageGPA = data['gradeDistribution']['AVG(averageGPA)'].toFixed(2)
+    //
+    //     //sends json as response
+    //     res.json({count: count, a: a, b: b, c: c, d: d, f: f, p: p, np: np,
+    //               averageGPA: averageGPA, classes: classes, instructors: instructors,
+    //               instructor: params.instructor, quarters: params.quarters,
+    //               department: params.department, classNumber: params.classNumber,
+    //               classCode: params.classCode, courseList: data['courseList']
+    //     });
+    // });
 })
-
 
 
 
